80999ef012cb96188d52f6729be4903dfd59974e
Dan Riding <doonyby@gmail.com>
2016-11-18 07:53:51 -0700

    finished filters and logic

--- module.js
+++ module.js
@@ -8,21 +8,41 @@ angular.module('myApp', [])
 			if( vm.myForm.$valid ) {
 				console.log('form is valid');
 				vm.mealCount++;
-				vm.redBox = false;
 				var tax = vm.taxRate/100;
 				var tip = vm.tipPercent/100;
-
 				vm.custSubtotal = (vm.mealPrice * tax) + vm.mealPrice;
 				vm.custTip = vm.custSubtotal * tip;
 				vm.custTotal = vm.custSubtotal + vm.custTip;
 				vm.tipTotal += vm.custTip
-
 				vm.avgTip = (vm.tipTotal / vm.mealCount);
-
+				vm.mealPrice = "";
+				vm.taxRate = "";
+				vm.tipPercent = "";
+				vm.myForm.$setPristine();
 			} else {
 				console.log('form is not valid');
 				vm.redBox = true;
 			}
+		};
+		vm.cancelMeal = function() {
+			vm.myForm.$setPristine();
+			vm.redBox = false;
+			vm.mealPrice = "";
+			vm.taxRate = "";
+			vm.tipPercent = "";
+		};
+		vm.reset = function() {
+			vm.redBox = false;
+			vm.mealCount = 0;
+			vm.tipTotal = 0;
+			vm.mealPrice = "";
+			vm.taxRate = "";
+			vm.tipPercent = "";
+			vm.custSubtotal = "";
+			vm.custTip = "";
+			vm.custTotal = "";
+			vm.tipTotal = "";
+			vm.avgTip = "";			
 		};
 	});
 
